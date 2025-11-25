d2215fcb16417e298a586d67da9a902e9ee94d87
Minal chouhan <chouhanminal4@gmail.com>
2025-11-25 12:13:22 +0530

    comment

--- equalizedarray.cpp
+++ equalizedarray.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// tc=o(n)  sc=o(1)
 int equalizeArray(vector<int> arr) {
 vector<int> freq(100,0);
 int size = arr.size();
@@ -11,6 +11,22 @@ int max = *max_element(freq.begin(),freq.end());
 
 return size-max; 
 }
+// optimized code
+// int equalizeArray(vector<int> arr) {
+//     unordered_map<int,int> freq;
+//     int size = arr.size();
+
+//     for(int x : arr){
+//         freq[x]++;
+//     }
+
+//     int maxFreq = 0;
+//     for(auto &p : freq){
+//         maxFreq = max(maxFreq, p.second);
+//     }
+
+//     return size - maxFreq;
+// }
 
 
 int main(){
